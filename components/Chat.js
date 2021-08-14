0cdeecfde1655a1633d441d3f9da5ad139d09dd9
Nicholas Lamb <nickplamb@gmail.com>
2021-08-14 18:47:17 -0400

    add app routing, default to Start component and link to chat page.

--- components/Chat.js
+++ components/Chat.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { Button, Text, View, StyleSheet } from 'react-native';
 
 export default class Chat extends Component {
+  
+  componentDidMount() {
+    this.props.navigation.setOptions({ title: this.props.route.params.name });
+  }
 
   render() {
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+    
     return (
       <View style={ styles.containter }>
         <Text>Hello { name }</Text>
