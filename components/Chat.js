1fd8eb951f444737730fce78b913b5ac4177934c
Nicholas Lamb <nickplamb@gmail.com>
2021-08-17 19:24:53 -0400

    connect chat to firebase with listeners

--- components/Chat.js
+++ components/Chat.js
@@ -1,55 +1,86 @@
 import React, { Component } from 'react';
-import { KeyboardAvoidingView, Platform, View, StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Platform, View, StyleSheet, Text } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 export default class Chat extends Component {
   constructor(props) {
     super(props);
     this.state = {
       messages: [],
+      uid: '',
     }
-  }
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCV3EFBY9Qir3KZsoScbqDQc2SpcJbwEFc",
+      authDomain: "chat-app-d5e57.firebaseapp.com",
+      projectId: "chat-app-d5e57",
+      storageBucket: "chat-app-d5e57.appspot.com",
+      messagingSenderId: "84291303167",
+      appId: "1:84291303167:web:fcc3d29b3ad5c6514a7eca",
+      measurementId: "G-W0PPFEK4WE"
+    };
+
+    // Initialize Firebase
+    if(!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+      // firebase.analytics(); 
+    }
+    
+    // reference to messages collection on firebase
+    this.referenceMessages = firebase.firestore().collection('messages');
+  };
 
   componentDidMount() {
     let name = this.props.route.params.name
     // I put this in componentDidMount because of a warning in react 16+ https://reactjs.org/blog/2020/02/26/react-v16.13.0.html#warnings-for-some-updates-during-render
     this.props.navigation.setOptions({ title: name });
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: `Hello ${name}!`,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: `${name} just joined the chat`,
-          createdAt: new Date(),
-          system: true,
-        },
-        {
-          _id: 3,
-          text: 'Hello Developers',
-          createdAt: new Date(),
-          user: {
-            _id: 5,
-            name: 'React',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ]
-    })
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+      // if the user has not been registered, register them.
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+
+      this.setState({
+        uid: user.uid,
+      });
+
+      // listen for changes to the messages collection
+      this.unsubscribeMessages = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
+    });
+  };
+
+  componentWillUnmount() {
+    // unsubscribe both listeners
+    this.unsubscribeMessages();
+    this.authUnsubscribe();
   }
 
-  onSend(messages = []){
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+  onCollectionUpdate = querySnapshot => {
+    const messages = [];
+    querySnapshot.forEach(doc => {
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+
+    // sort messages by createdAt date
+    messages.sort((a, b) => b.createdAt - a.createdAt);
+    this.setState({
+      messages: messages,
+    });
+  }
+
+  addMessage(message = {}){
+    // send new message to firebase
+    this.referenceMessages.add(message[0])
   }
 
   renderBubble(props) {
@@ -61,7 +92,7 @@ export default class Chat extends Component {
             backgroundColor: '#000',
           },
           left: {
-            backgroundColor: '#abc'
+            backgroundColor: '#FFF'
           }
         }}
       />
@@ -75,11 +106,13 @@ export default class Chat extends Component {
     return (
         <View style={ styles.containter }>
           <GiftedChat
-            renderBubble={this.renderBubble.bind(this)}
-            messages={this.state.messages}
-            onSend={ messages => this.onSend(messages) }
+            renderBubble={ this.renderBubble.bind(this) }
+            messages={ this.state.messages }
+            onSend={ message => this.addMessage(message) }
             user={{
-              _id: 1,
+              _id: this.state.uid,
+              name: name,
+              avatar: 'https://placeimg.com/140/140/any',
             }} 
             messagesContainerStyle={{ backgroundColor: backgroundColor }}
           />
